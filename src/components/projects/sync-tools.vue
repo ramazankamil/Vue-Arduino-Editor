e06b3b05e130f9aef6399a9e510d39276323c4af
Milyaz <milyazkamil@gmail.com>
2025-08-03 13:13:19 +0300

    Added postMessage functionality.

--- src/components/projects/sync-tools.vue
+++ src/components/projects/sync-tools.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <v-row class="px-3">
     <v-col cols="auto">
       <v-tooltip top>
@@ -73,6 +73,140 @@ export default {
       this.exportLoading = true;
       await this.$bundler.exportProject(this.project);
       this.exportLoading = false;
+    },
+  },
+};
+</script> -->
+
+
+<template>
+  <v-row class="px-3">
+    <v-col cols="auto">
+      <v-tooltip top>
+        <template #activator="{ on }">
+          <v-btn v-show="!!project.uuid" icon :loading="exportLoading" v-on="on" @click="exportProject">
+            <v-icon>mdi mdi-folder-download-outline</v-icon>
+          </v-btn>
+        </template>
+        <span>Export Project (Save)</span>
+      </v-tooltip>
+    </v-col>
+    
+    <v-col cols="auto">
+      <v-tooltip top>
+        <template #activator="{ on }">
+          <v-btn v-show="!!project.uuid" icon :loading="saveToEtEduLoading" v-on="on" @click="saveToEtEdu">
+            <v-icon>mdi-content-save-move-outline</v-icon>
+          </v-btn>
+        </template>
+        <span>Save to Et-Edu</span>
+      </v-tooltip>
+    </v-col>
+    
+    <v-col cols="auto">
+      <save-project import-project>
+        <template #activator="{ on: onSave }">
+          <v-tooltip top>
+            <template #activator="{ on: onTooltip }">
+              <v-btn icon v-on="{ ...onTooltip, ...onSave }">
+                <v-icon>mdi mdi-folder-upload-outline</v-icon>
+              </v-btn>
+            </template>
+            <span>Import Project (Open)</span>
+          </v-tooltip>
+        </template>
+      </save-project>
+    </v-col>
+    <v-col cols="auto">
+      <v-tooltip top>
+        <template #activator="{ on }">
+          <v-badge :value="!!libs" :content="libs" overlap>
+            <v-btn icon to="/tools/libraries" v-on="on">
+              <v-icon>mdi mdi-book-open-variant</v-icon>
+            </v-btn>
+          </v-badge>
+        </template>
+        <div v-if="libs" class="text-center">
+          <strong>Libraries</strong>
+          <div v-for="(lib, i) in project.libraries" :key="i">
+            {{lib.name}} - {{lib.version}}
+          </div>
+        </div>
+        <strong v-else>Libraries</strong>
+      </v-tooltip>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { zip, strToU8 } from 'fflate';
+import SaveProject from './save-project.vue';
+
+export default {
+  components: {
+    SaveProject,
+  },
+  props: {
+    project: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      exportLoading: false,
+      saveToEtEduLoading: false,
+    };
+  },
+  computed: {
+    libs() {
+      return this.project?.libraries?.length;
+    },
+  },
+  methods: {
+    async exportProject() {
+      this.exportLoading = true;
+      await this.$bundler.exportProject(this.project);
+      this.exportLoading = false;
+    },
+    async saveToEtEdu() {
+      if (!this.project) return;
+      this.saveToEtEduLoading = true;
+      try {
+        const { File } = this.$FeathersVuex.api;
+        const { data: files } = File.findInStore({ query: { projectId: this.project.uuid } });
+
+        // Prepare files for zipping, using the relative path as the key
+        const fileObj = files.reduce((acc, file) => {
+          const path = file.ref.replace(`${this.project.ref}/`, '');
+          acc[path] = strToU8(file.body); // Convert file content to Uint8Array
+          return acc;
+        }, {});
+
+        // Zip the files using fflate
+        const zipped = await new Promise((resolve, reject) => {
+          zip(fileObj, (err, res) => (err ? reject(err) : resolve(res)));
+        });
+
+        // Create a Blob from the zipped data
+        const zipBlob = new Blob([zipped.buffer], { type: 'application/zip' });
+        const projectFilename = `${this.project.ref}.zip`;
+
+        // Send the data to the parent window
+        window.parent.postMessage({
+          action: 'saveArduinoProjectToBackend',
+          data: {
+            fileBlob: zipBlob,
+            projectFilename,
+          }
+        }, '*'); // For better security, replace '*' with your website's origin
+
+      } catch (error) {
+        console.error('Failed to save project to Et-Edu:', error);
+        // You could add user-facing error feedback here
+      } finally {
+        this.saveToEtEduLoading = false;
+      }
     },
   },
 };
