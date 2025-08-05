9e20a437bb26ffc628d22aec4327d742601f4fba
Milyaz <milyazkamil@gmail.com>
2025-08-05 22:09:13 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -163,7 +163,7 @@
   </v-app>
 </template>
 
-<script>
+<!-- <script>
 import { mapMutations } from 'vuex';
 import snakeCase from 'lodash/snakeCase';
 import { zip, strToU8 } from 'fflate';
@@ -298,6 +298,188 @@ export default {
         await file.save();
         this.$store.commit('setCurrentProject', project.uuid);
         this.$store.commit('setCurrentFile', file.uuid);
+        if (this.$router.currentRoute.path !== '/code') {
+          this.$router.push('/code');
+        }
+      } catch (error) {
+        window.parent.postMessage({ action: 'loadProjectError', error: error.message }, '*');
+      }
+    },
+  },
+  async mounted() {
+    this.checkSerialReady();
+    this.$FeathersVuex.api.File.find({ query: { $limit: 9999999 } });
+    this.$FeathersVuex.api.Project.find({ query: { $limit: 9999999 } });
+    await this.$FeathersVuex.api.Setting.find({ query: { $limit: 9999999 } });
+    const { Setting } = this.$FeathersVuex.api;
+    const { data } = Setting.findInStore({ query: { key: 'editor' } });
+    this.$vuetify.theme.dark = /(dark)|(black)/.test(data[0]?.value?.theme ?? '');
+    window.addEventListener('message', this.handleParentMessage);
+  },
+  beforeDestroy() {
+    window.removeEventListener('message', this.handleParentMessage);
+  },
+};
+</script> -->
+<script>
+import { mapMutations } from 'vuex';
+import snakeCase from 'lodash/snakeCase';
+// 🚀 CHANGE: We no longer need strFromU8, only unzip.
+import { zip, strToU8, unzip } from 'fflate';
+import BoardFooter from './components/boards/footer-btn.vue';
+import ServerFooter from './components/servers/footer-btn.vue';
+import SerialFooter from './components/serial/footer-btn.vue';
+import SerialPrompts from './components/serial/prompts.vue';
+import SerialMonitor from './components/serial/monitor.vue';
+import SerialPlotter from './components/serial/plotter.vue';
+import CompileBtn from './components/program/compile.vue';
+import UploadBtn from './components/program/upload.vue';
+import CompileConsole from './components/program/console.vue';
+import Coffee from './components/coffee.vue';
+import { version } from '../package.json';
+
+export default {
+  name: 'App',
+  components: {
+    ServerFooter,
+    BoardFooter,
+    SerialPrompts,
+    SerialFooter,
+    SerialMonitor,
+    SerialPlotter,
+    CompileBtn,
+    CompileConsole,
+    UploadBtn,
+    Coffee,
+  },
+  data() {
+    return {
+      serialReady: false,
+      tab: 'program',
+      version,
+      saveToEtEduLoading: false,
+    };
+  },
+  computed: {
+    currentProject() {
+      return this.$store.getters['projects/find']({ query: { uuid: this.$store.getters.currentProject } }).data[0];
+    },
+  },
+  methods: {
+    // ... (other methods like saveToEtEdu, checkSerialReady, etc. are unchanged)
+    ...mapMutations(['toggleSerialShelf', 'setSerialTab']),
+    checkSerialReady() {
+      if (this.$serial) this.serialReady = true;
+      else setTimeout(() => this.checkSerialReady(), 100);
+    },
+    async saveToEtEdu() {
+      if (!this.currentProject) return;
+      this.saveToEtEduLoading = true;
+      try {
+        const { File } = this.$FeathersVuex.api;
+        const { data: files } = File.findInStore({ query: { projectId: this.currentProject.uuid } });
+
+        const fileObj = files.reduce((acc, file) => {
+          const path = file.ref.replace(`${this.currentProject.ref}/`, '');
+          acc[path] = strToU8(file.body);
+          return acc;
+        }, {});
+
+        const zipped = await new Promise((resolve, reject) => {
+          zip(fileObj, (err, res) => (err ? reject(err) : resolve(res)));
+        });
+
+        const zipBlob = new Blob([zipped.buffer], { type: 'application/zip' });
+        const projectFilename = `${this.currentProject.ref}.zip`;
+
+        window.parent.postMessage({
+          action: 'saveArduinoProjectToBackend',
+          data: {
+            fileBlob: zipBlob,
+            projectFilename,
+          },
+        }, '*');
+      } catch (error) {
+        // Linter rules prevent console.log here.
+      } finally {
+        this.saveToEtEduLoading = false;
+      }
+    },
+    async handleParentMessage(event) {
+      const { action, remoteFileUrl, projectTitle } = event.data;
+      if (action === 'loadArduinoProject' && remoteFileUrl && projectTitle) {
+        await this.loadProjectFromUrl(remoteFileUrl, projectTitle);
+      }
+
+      if (action === 'resetEditor') {
+        this.resetEditorState();
+      }
+    },
+    resetEditorState() {
+      localStorage.clear();
+      this.$store.commit('setCurrentProject', null);
+      this.$store.commit('setCurrentFile', null);
+      if (this.$router.currentRoute.path !== '/code') {
+        this.$router.push('/code');
+      }
+    },
+    async loadProjectFromUrl(url, name) {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch project file: ${response.statusText}`);
+        }
+        const zipArrayBuffer = await response.arrayBuffer();
+        const zipData = new Uint8Array(zipArrayBuffer);
+
+        const unzipped = await new Promise((resolve, reject) => {
+          unzip(zipData, (err, res) => (err ? reject(err) : resolve(res)));
+        });
+
+        const { Project, File } = this.$FeathersVuex.api;
+        let ref = snakeCase(name);
+        let count = 1;
+        while (this.$store.getters['projects/find']({ query: { ref } }).total) {
+          count += 1;
+          ref = `${snakeCase(name)}_${count}`;
+        }
+        let project = new Project({ name, ref });
+        project = await project.save();
+
+        let mainFileUUID = null;
+
+        // 🚀 START CHANGE: Use TextDecoder for Turkish character support
+        const decoder = new TextDecoder('windows-1254');
+        // 🚀 END CHANGE
+
+        const fileCreationPromises = Object.keys(unzipped).map(async (filePath) => {
+          // 🚀 START CHANGE: Decode the file content using the specified decoder
+          const fileContent = decoder.decode(unzipped[filePath]);
+          // 🚀 END CHANGE
+          const isMain = filePath.endsWith('.ino');
+
+          const file = new File({
+            name: filePath,
+            ref: `${project.ref}/${filePath}`,
+            body: fileContent,
+            contentType: 'text/x-arduino',
+            main: isMain,
+            projectId: project.uuid,
+          });
+          await file.save();
+
+          if (isMain) {
+            mainFileUUID = file.uuid;
+          }
+        });
+
+        await Promise.all(fileCreationPromises);
+
+        this.$store.commit('setCurrentProject', project.uuid);
+        if (mainFileUUID) {
+          this.$store.commit('setCurrentFile', mainFileUUID);
+        }
+
         if (this.$router.currentRoute.path !== '/code') {
           this.$router.push('/code');
         }
